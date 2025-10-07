1390b580067031ba8074802c33a69ef8037dccb4
Henrique Sá <hinriqui@outlook.com.br>
2025-10-07 15:29:13 -0300

    Add comentários ao código Js

--- Exercicio-03/script.js
+++ Exercicio-03/script.js
@@ -1,14 +1,19 @@
+
+//comecei pegando os elementos do html que adicionei uma ID
 let Nome = document.getElementById("addNome");
 let Idade = document.getElementById("addIdade");
 let Cidade = document.getElementById("addCidade");
 let Botao = document.getElementById("botao");
 let CorpoTabela = document.getElementById("tabela");
 
+//agora dei vida ao meu botão usando addEventListener com click, e peguei os valores dos meus campos.
 Botao.addEventListener('click', function() {
  let nomeValor = Nome.value.trim();
     let idadeValor = Idade.value.trim();
     let cidadeValor = Cidade.value.trim();
 
+    /*usei um if else, comecei verificando se o usuario não deixou algum campo em branco
+    depois no else usei para criar as celulas que recebem as entradas do usuario*/
     if (nomeValor === "" || idadeValor === "" || cidadeValor === "") {
         alert("Preencha todos os campos!");
     } else {
@@ -24,12 +29,15 @@ Botao.addEventListener('click', function() {
         let celulaCidade = document.createElement('td');
         celulaCidade.textContent = cidadeValor;
 
+        //criando a nova linha com as entradas
        novaLinha.appendChild(celulaNome);
        novaLinha.appendChild(celulaIdade);
        novaLinha.appendChild(celulaCidade);
 
+       //adicionando a linha a tabela
         CorpoTabela.appendChild(novaLinha);
-
+ 
+        // pra finalizar vem meu faxineiro limpar os campos de texto
         Nome.value = "";
         Idade.value = "";
         Cidade.value = "";
